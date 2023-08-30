dc360e582ee862f2c53a8d97a3e2f108ecad5022
eqxonee <lich1997a@yandex.ru>
2023-08-30 10:45:32 +0300

    done kafka + brokers + customJarDto2

---
+++ SampleDto/src/main/java/com/example/sampledto/SampleDtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampledto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleDtoApplication.class, args);
+    }
+
+}
