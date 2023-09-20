dee461d3b34b79d73de5b4b66c626f4cc6d8c8b4
eqxonee <lich1997a@yandex.ru>
2023-09-20 18:51:36 +0300

    start kafka processor2

---
+++ BankProcessor/src/main/java/com/example/bankprocessor/BankProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankProcessorApplication.class, args);
+    }
+
+}
