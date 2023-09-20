8b99613cc679d52473efe87384a6ecc634e4273e
eqxonee <lich1997a@yandex.ru>
2023-09-20 22:06:20 +0300

    start kafka processor4

---
+++ BankProcessor/src/test/java/com/example/bankprocessor/BankProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
